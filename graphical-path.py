931347ff80a0862b3b65aa18aa43ded00a360965
Ribeiro <wesley.ribeiro.ext@siemens.com>
2019-09-18 19:35:51 -0300

    Changed colors

--- graphical-path.py
+++ graphical-path.py
@@ -8,14 +8,16 @@ def on_click(event):
 
     canvas = event.widget
 
+    # TODO: friendly colors
     if click_counter == 0:
-        current_color = 'red'
+        current_color = '#1b870f'
     elif click_counter == 1:
-        current_color = 'green'
+        current_color = '#a8050a'
     else:
-        current_color = 'black'
+        current_color = '#659df7'
 
     if 'filled' not in canvas.gettags(CURRENT):
+        # TODO: fix this
         # This just looks awful, let's get rid of it
         tags = list(canvas.gettags(CURRENT))
         tags.append("filled")
@@ -23,20 +25,19 @@ def on_click(event):
         canvas.itemconfig(CURRENT, fill=current_color, tags=tags)
         click_counter += 1
 
+
 def main():
     root = Tk()
 
     root.title("Pathfinding project")
     root.geometry("800x600")
-    print(root.winfo_geometry())
 
     canvas = Canvas(root, width=800, height=600)
 
-
     # TODO: Make this more generic
-    for x in range(0, 800, 20):
-        for y in range(0, 600, 15):
-            canvas.create_rectangle(x, y, x + 20, y + 15, fill='white')
+    for x in range(0, 800, 40):
+        for y in range(0, 600, 20):
+            canvas.create_rectangle(x, y, x + 40, y + 20, outline='#e1f6fa', fill='#a8eaf7')
 
     canvas.bind("<Button-1>", on_click)
 
