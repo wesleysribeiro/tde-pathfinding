28709ef5ce359ecea67fc5525a0fc51aee42e886
Ribeiro <wesley.ribeiro.ext@siemens.com>
2019-09-18 19:01:36 -0300

    Fixing things

--- graphical-path.py
+++ graphical-path.py
@@ -1,32 +1,46 @@
 from tkinter import *
-from tkinter import ttk
+
+click_counter = 0
 
 
-def callback(self):
-    self.configure(bg="red")
-    # button
+def on_click(event):
+    global click_counter
 
+    canvas = event.widget
 
-def draw_scene(root, buttons):
-    for row in range(0, 10):
-        for col in range(0, 10):
-            button = Button(root, text="Button" + str(row*10 + col))
-            button.config(command=lambda: callback(button))
-            button.grid(row=row, column=col)
+    if click_counter == 0:
+        current_color = 'red'
+    elif click_counter == 1:
+        current_color = 'green'
+    else:
+        current_color = 'black'
 
+    if 'filled' not in canvas.gettags(CURRENT):
+        # This just looks awful, let's get rid of it
+        tags = list(canvas.gettags(CURRENT))
+        tags.append("filled")
+        tags = tuple(tags)
+        canvas.itemconfig(CURRENT, fill=current_color, tags=tags)
+        click_counter += 1
 
 def main():
     root = Tk()
+
     root.title("Pathfinding project")
     root.geometry("800x600")
+    print(root.winfo_geometry())
 
-    draw_scene(root)
+    canvas = Canvas(root, width=800, height=600)
 
-    # button = Button(root, text="Testing")
-    # button.pack()
-    # button.bind("<Button-3>", callback)
+    for x in range(0, 800, 20):
+        for y in range(0, 600, 15):
+            canvas.create_rectangle(x, y, x + 20, y + 15, fill='white')
 
+    canvas.bind("<Button-1>", on_click)
+
+    canvas.pack()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
