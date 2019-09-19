38380d5ca6abd9ffe30a30a771314d21a85b83bb
Wesley Ribeiro <wesleysribeiro97@gmail.com>
2019-09-18 23:02:50 -0300

    Added OptionMenus

--- graphical-path.py
+++ graphical-path.py
@@ -6,7 +6,7 @@ click_counter = 0
 FILLED_TAG = 'filled'
 
 
-# This will be useful somehow
+# This will be useful somehow, someday
 class Click(Enum):
     ORIGIN = 0,
     DESTINY = 1,
@@ -44,18 +44,34 @@ def draw_scene(canvas, width, height):
             canvas.create_rectangle(x, y, x + step_x, y + step_y, outline='#c9f6ff', fill='#a8eaf7')
 
 
+# Draws the given matrix into the scene
+# def draw_path(matrix):
+# TODO: Implement this
+
+
 def main():
     root = Tk()
 
     root.title("Pathfinding project")
     window_geometry = {'width': 800, 'height': 600}
-    root.geometry(str(window_geometry['width'])+"x"+str(window_geometry['height'])) # convert to string "widthxheight"
+
+    root.geometry(str(window_geometry['width'])+"x"+str(window_geometry['height'] + 150)) # convert to string "widthxheight"
 
     canvas = Canvas(root, width=window_geometry['width'], height=window_geometry['height'])
     draw_scene(canvas, window_geometry['width'], window_geometry['height'])
     canvas.bind("<Button-1>", on_click)
 
+    frame = Frame(root, bg='#dff5f1', height=200, width=window_geometry['width'])
     canvas.pack()
+    menu_options = StringVar(frame)
+    menu_options.set("10x10")
+    select_matrix = OptionMenu(frame, menu_options, "10x10", "20x20", "30x30")
+    select_matrix.grid(row=0, column=0, padx=20)
+    select_algorithm = OptionMenu(frame, "A*", "Dijkstra", "Three", "Four")
+    select_algorithm.grid(row=0, column=1, padx=20)
+    submit_button = Button(frame, text='Submit', state=DISABLED)
+    submit_button.grid(row=0, column=2, padx=20)
+    frame.pack()
     root.mainloop()
 
 
